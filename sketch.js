32e22c56bed17d9ce4bc352ff78b6282014a482e
Mikkel Kokholm <mikkel@kokholm.dk>
2024-12-02 14:19:40 +0100

    api

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1920 , 1080);
+  loadJSON("https://ipgeolocation.abstractapi.com/v1?api_key=3ed637e3f8c24a1eb43b916fbc212d78", t => console.log(t));
 }
 
 function draw() {
