a0e42ffab6bbb09e82f291efbe90a381f51736eb
MarcShade <marcsorensenmail@gmail.com>
2025-01-13 15:08:51 +0100

    merged main

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
+let longitude;
+let latitude;
+
+function processData(data) {
+  longitude = data.longitude
+  latitude = data.latitude
+  console.log("Longitude: " + longitude)
+  console.log("Latitude: " + latitude)
+
+  loadJSON(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`, data => console.log(data));
+}
+
 function setup() {
-  loadJSON("https://ipgeolocation.abstractapi.com/v1?api_key=3ed637e3f8c24a1eb43b916fbc212d78", t => console.log(t));
-  loadJSON("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=1", b => console.log(b));
-  
+  loadJSON("https://ipgeolocation.abstractapi.com/v1?api_key=3ed637e3f8c24a1eb43b916fbc212d78", processData);
+  createCanvas(900, 1600);
+  background(color(50, 59, 74));
 }
 
 function draw() {
-  background(145, 191, 166);
+
 }
